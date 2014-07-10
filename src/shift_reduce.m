ec6acdc12df269802762711f01b1430be8df1204
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-10 16:24:14 +0200

    created compiling library stub

--- src/shift_reduce.m
+++ src/shift_reduce.m
@@ -1,39 +1,23 @@
 %----------------------------------------------------------------------------%
 % vim: ft=mercury ff=unix ts=4 sw=4 et
 %----------------------------------------------------------------------------%
-% File: egt.m
+% File: shift_reduce.m
 % Copyright © 2014 Sebastian Godelet
 % Main author: Sebastian Godelet <sebastian.godelet+github@gmail.com>
-% Created on: Thu Jul 10 16:17:57 CEST 2014
+% Created on: Thu Jul 10 16:11:22 CEST 2014
 % Stability: low
 %----------------------------------------------------------------------------%
-% egt is responsible for loading the enhanced grammar table binary format.
+% Top-level module of the shift-reduce parser library.
 %----------------------------------------------------------------------------%
 
-:- module shift_reduce.egt.
+:- module shift_reduce.
 
 :- interface.
 
-:- import_module int.
-:- import_module io.
+:- include_module shift_reduce.egt.
 
 %----------------------------------------------------------------------------%
 
-:- type grammar_tables
-    ---> grammar_tables.
-
-:- pred read_tables(io.binary_input_stream::in, grammar_tables::out,
-    io::di, io::uo) is det.
-
 %----------------------------------------------------------------------------%
-%----------------------------------------------------------------------------%
-
-:- implementation.
-
-%----------------------------------------------------------------------------%
-
-read_tables(Input, grammar_tables, !IO).
-
-%----------------------------------------------------------------------------%
-:- end_module shift_reduce.egt.
+:- end_module shift_reduce.
 %----------------------------------------------------------------------------%
