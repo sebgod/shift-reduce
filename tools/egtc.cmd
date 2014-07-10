65b938af842a06c327007684ab14697de9730a95
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-10 13:29:12 +0200

    hardening egtc arguments

--- tools/egtc.cmd
+++ tools/egtc.cmd
@@ -2,4 +2,21 @@
 @set ROOT=%~dp0~~~
 @set ROOT=%ROOT:\~~~=%
 @set ROOT=%ROOT:\=/%
-@call make -f "%~dp0Makefile" ROOT="%ROOT%" %*
+@set FILE=%~f1
+@set EXT=%~x1
+@set EXT_VALID=0
+@set VALID_EXTS=.grm .egt
+@for %%E in (%VALID_EXTS%) do @if /i "%EXT%" == "%%E" set EXT_VALID=1
+
+@if defined FILE @(
+    if %EXT_VALID% EQU 0 (
+        echo The extension "%EXT%" is not supported by %~n0, please choose one of %VALID_EXTS% 1>&2
+        exit /b 1
+    )
+
+    if /i "%EXT%" EQU ".grm" set FILE=%~dp1%~n1.egt
+    call make -f "%~dp0Makefile" ROOT="!ROOT!" "!FILE!"
+) else (
+    echo Please specify a grammar file to be compiled or an egt file to be recompiled 1>&2
+    exit /b 1
+)
