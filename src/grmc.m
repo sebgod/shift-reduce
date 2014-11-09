c5f84d561d8e51880db17e7db81a42c5ec198c59
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-11-09 23:01:37 +0800

    tests/test_shift_reduce.m -> src/grmc.m: moved compiler procedures to grmc sub-module of the library

--- src/grmc.m
+++ src/grmc.m
@@ -14,7 +14,71 @@
 
 :- interface.
 
+:- import_module io.
+
+    % progdir(ProgDir, !IO):
+    %
+    % Strips the name of the executable from io.progname/4.
+    %
+:- pred progdir(string::out, io::di, io::uo) is det.
+
+    % compiler_executable(Executable, !IO)
+    %
+    % Executable is the base name of the compiler executable as needed for
+    % io.system_call/4.
+    %
+:- pred compiler_executable(string::out, io::di, io::uo) is det.
+
+    % compile(BaseDir, GrammarFile, !IO)
+    %
+    % Based on the BaseDir, the compiler executable is executed with the
+    % GrammarFile as the argument, it should be an absolute path.
+    %
+    % The predicate will succeed if the command line could execute
+    % successfully, and will through an exception otherwise.
+    %
+:- pred compile(string::in, string::in, io::di, io::uo) is det.
+
 %----------------------------------------------------------------------------%
+%----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module dir.
+:- import_module list.
+:- import_module maybe.
+:- import_module require.
+:- import_module string.
+
+%----------------------------------------------------------------------------%
+
+progdir(ProgDir, !IO) :-
+    io.progname("", ProgName, !IO),
+    ProgDir = dirname(ProgName).
+
+compiler_executable(Executable, !IO) :-
+    io.get_environment_var("OS", MaybeOS, !IO),
+    Executable = ( MaybeOS = yes("Windows_NT") ->  "grmc.cmd" ; "grmc" ).
+
+
+compile(BaseDir, GrammarFile, !IO) :-
+    compiler_executable(CompilerExe, !IO),
+    CompileCmd = ( BaseDir / CompilerExe ) ++ " " ++ GrammarFile,
+    FailMsg = (pred(Fmt::in, Args::in) is erroneous :-
+        unexpected($file, $pred, format("compile [%s] failed: " ++ Fmt,
+                    [s(CompileCmd)] ++ Args))),
+    io.call_system(CompileCmd, CompileResult, !IO),
+    ( CompileResult = ok(ExitCode) ->
+        ( ExitCode \= 0 ->
+            FailMsg("error code: %d", [i(ExitCode)])
+        ;
+            true
+        )
+    ; CompileResult = error(CompileError) ->
+        FailMsg("io: %s", [s(io.error_message(CompileError))])
+    ;
+        FailMsg("unknown io.call_system/4 result", [])
+    ).
 
 %----------------------------------------------------------------------------%
 :- end_module shift_reduce.grmc.
--- tests/test_shift_reduce.m
+++ tests/test_shift_reduce.m
@@ -27,32 +27,20 @@
 
 :- import_module shift_reduce.
 :- import_module shift_reduce.egt.
+:- import_module shift_reduce.grmc.
 :- import_module dir.
 :- import_module list.
-:- import_module maybe.
 :- import_module require.
 :- import_module string.
 
 %----------------------------------------------------------------------------%
 
 main(!IO) :-
-    io.get_environment_var("OS", MaybeOS, !IO),
-    io.progname("", ProgName, !IO),
-    ProgDir = dirname(ProgName),
-    BaseDir = ProgDir / ".." / "tools",
-    CompileExe = ( MaybeOS = yes("Windows_NT") ->  "grmc.cmd" ; "grmc" ),
-    GrammarFile = "ParserTest.grm",
-    CompiledGrammarFile = "ParserTest.egt",
-    CompileCmd = ( BaseDir / CompileExe ) ++ " " ++ ( ProgDir / GrammarFile ),
-    io.call_system(CompileCmd, CompileResult, !IO),
-    ( CompileResult = ok(ExitCode) ->
-        io.format("%s result: %d\n", [s(CompileCmd), i(ExitCode)], !IO)
-    ; CompileResult = error(CompileError) ->
-        unexpected($file, $pred, "could not compile grammar file: " ++
-            io.error_message(CompileError))
-    ;
-        unexpected($file, $pred, "unknown io.call_system/4 result")
-    ),
+    progdir(ProgDir, !IO),
+    ParserTestGrammar = "ParserTest",
+    GrammarFile = ProgDir / (ParserTestGrammar ++ ".grm"),
+    CompiledGrammarFile = ParserTestGrammar ++ ".egt",
+    compile(ProgDir / ".." / "tools", GrammarFile, !IO),
     io.format("Testing %s\n", [s(CompiledGrammarFile)], !IO),
     io.open_binary_input(CompiledGrammarFile, OpenResult, !IO),
     ( OpenResult = ok(FileInput) ->
@@ -68,5 +56,3 @@ main(!IO) :-
 %----------------------------------------------------------------------------%
 :- end_module test_shift_reduce.
 %----------------------------------------------------------------------------%
-
-
