72f0ceb73dc51d4b350f5605eee41a55b0f80759
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-11-19 00:00:36 +0800

    tests/tests: reading in ParserTest.txt

--- tests/test_shift_reduce.m
+++ tests/test_shift_reduce.m
@@ -32,8 +32,25 @@
 :- import_module dir.
 :- import_module list.
 :- import_module require.
+:- import_module string.
 
 %----------------------------------------------------------------------------%
+
+:- use_module stream.
+
+:- pred lex(text_input_stream::in, io::di, io::uo) is det.
+
+lex(Reader, !IO) :-
+    stream.get(Reader, Result, !IO),
+    ( if Result = stream.ok(Char) then
+        io.format("%c", [c(Char)], !IO)
+    else if Result = stream.error(Error) then
+        io.format("error: %s", [s(io.error_message(Error))], !IO)
+    else if Result = stream.eof then
+        true
+    else
+        unexpected($file, $pred, "unknown stream result")
+    ).
 
 main(!IO) :-
     progdir(ProgDir, !IO),
@@ -43,7 +60,15 @@ main(!IO) :-
         [force_recompile],
         !IO),
     from_file(EgtFile, Grammar, !IO),
-    io.write_line(Grammar, !IO).
+    open_input(ProgDir / "ParserTest.txt", OpenResult, !IO),
+    ( if OpenResult = ok(InputStream) then
+        lex(InputStream, !IO),
+        close_input(InputStream, !IO)
+    else if OpenResult = error(OpenError) then
+        unexpected($file, $pred, error_message(OpenError))
+    else
+        unexpected($file, $pred, "unknown `io.res(T)' value")
+    ).
 
 %----------------------------------------------------------------------------%
 :- end_module test_shift_reduce.
